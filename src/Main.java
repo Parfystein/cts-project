3aa97942724944acc30461cd367ee793af9bbcad
Alexandru Parfente <aparfente@gmail.com>
2025-03-13 19:52:13 +0200

    Added attributes for class task

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import modules.Task;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hi");
-        System.out.println("hei");
-        System.out.println("hello world");
+        Task task1 = new Task("Test1", "test");
+        Task task2 = new Task("Test2", "test");
+        Task task3 = new Task("Test3", "test");
+        System.out.println(task1.getId() + " " + task2.getId() + " " + task3.getId());
     }
 
 }
--- src/modules/Task.java
+++ src/modules/Task.java
@@ -1,5 +1,35 @@
 package modules;
 
 public class Task {
+    private String title;
+    private String description;
+    private static int idCounter = 0;
+    private int idTicket;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.idTicket = ++idCounter;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return idTicket;
+    }
 
 }
